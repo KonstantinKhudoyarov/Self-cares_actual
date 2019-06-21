3a67384d45f265ff15fdff45dd367e854e0a40f1
KonstantinKhudoyarov <kostya040390@gmail.com>
2019-06-21 19:02:21 +0300

    add interactivity to header, redesign main-home, add adaptive to slider

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -6,3 +6,14 @@
         headerMenu.classList.toggle('header__nav_active');
     });
 })();
+
+(function scrollHeader() {
+    var header = document.querySelector('.header_home');
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 1) {
+            header.classList.add('header_home-active');
+        } else {
+            header.classList.remove('header_home-active');
+        }
+    })
+})();
